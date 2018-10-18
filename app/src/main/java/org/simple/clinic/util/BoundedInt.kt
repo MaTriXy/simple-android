1cc3ae4b7edf7011b0b2949225a682eac75e9bfe
Sanchita Agarwal <sanchita@uncommon.is>
2018-10-18 18:12:57 +0530

    Add Awaiting approval notification for Forgot PIN

--- app/src/main/java/org/simple/clinic/util/BoundedInt.kt
+++
@@ -1,9 +0,0 @@
-package org.simple.clinic.util
-
-data class BoundedInt(val lower: Int, val upper: Int) {
-  init {
-    if (upper < lower) {
-      throw AssertionError("Upper bound [$upper] should be >= lower bound [$lower]")
-    }
-  }
-}
